af830b7134028e2f60dc51c8b42bdd20737d205a
Azamat Shomuratov <106393064+shomuratov@users.noreply.github.com>
2023-07-11 16:06:03 +0500

    tamom

---
+++ script.js
@@ -0,0 +1,45 @@
+'use strict'
+const clockContainer = document.querySelector(".clock-container");
+const dateContainer = document.querySelector(".date-container");
+const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+const dateClock = setInterval(function dateTime() {
+    const today = new Date();
+    let date = today.getDate();
+    let day = weekDays[today.getDay()];
+    const month = monthNames[today.getMonth()];
+
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    dateContainer.innerHTML = `
+        <p>${ day }</p>
+        <p><span>${ date }</span></p>
+        <p>${ month }</p>`;
+
+    clockContainer.innerHTML = `${ hours } : ${ minutes }`;
+}, 1000)
